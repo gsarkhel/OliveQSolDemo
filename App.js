9bf25c9ea2f27399a7925bf63025b3eaaec3937b
Govinda Sarkhel <gsarkhel@gmail.com>
2024-11-29 13:27:10 +0530

    Fist draft

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import styles from './assets/styles';
+import {View, Text, Image, ScrollView, StatusBar} from 'react-native';
 
 const App = () => {
   return (
     <>
       <StatusBar hidden={true} />
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello, Fullscreen App!</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Image
+          source={require('./assets/img/logo.png')} // Replace with your local image path
+          style={styles.logopng}
+        />
+        <View style={styles.topcontainer}>
+          <View style={styles.innercontainer}>
+            <Text style={styles.textA}>
+              Powering the World of Digital Content and,
+            </Text>
+            <Text style={styles.textB}>Learning Platforms</Text>
+            <Text style={styles.textC}>
+              OliveQSol is a leading next-gen technology company that offers
+              educational content and learning solutions—leveraging digital,
+              process automation, and robust learning design principles.
+            </Text>
+          </View>
+          <Image
+            source={require('./assets/img/header_img.png')} // Replace with your local image path
+            style={styles.headerpng}
+          />
+        </View>
+      </ScrollView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  text: {
-    fontSize: 24,
-    color: '#333',
-  },
-});
 
 export default App;
---
+++ assets/styles.js
@@ -0,0 +1,58 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  scrollView: {
+    width: '100%',
+    height: '100%',
+  },
+  logopng: {
+    marginLeft: 50,
+    marginTop: 50,
+  },
+  topcontainer: {
+    width: '100%',
+    height: 550,
+    overflow: 'hidden',
+    marginTop: 50,
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#fcfcfd',
+  },
+  innercontainer: {
+    width: 550,
+  },
+  headerpng: {
+    width: 500,
+    height: 333,
+  },
+  textA: {
+    fontSize: 55,
+    fontFamily: 'Jost-Light',
+    // marginLeft: 50,
+    width: 550,
+    color: '#566c30',
+    // backgroundColor: '#ff0000',
+  },
+  textB: {
+    fontSize: 50,
+    fontFamily: 'Jost-SemiBold',
+    // left: 50,
+    // marginLeft: 50,
+    marginTop: 20,
+    width: 550,
+    color: '#566c30',
+    // backgroundColor: '#ff0000',
+  },
+  textC: {
+    fontSize: 18,
+    fontFamily: 'Jost-SemiBold',
+    // left: 50,
+    // marginLeft: 50,
+    marginTop: 20,
+    width: 550,
+    color: '#000000',
+    // backgroundColor: '#ff0000',
+  },
+});
